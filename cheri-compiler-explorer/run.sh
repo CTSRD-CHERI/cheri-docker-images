08d79aa029c7e0b98b21c828d92e270ba4502586
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-11-04 10:23:30 +0000

    Copy to the right file!

--- cheri-compiler-explorer/run.sh
+++ cheri-compiler-explorer/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+set -xe
+
 git pull --rebase --autostash
-mkdir -p ~/appdata/letsencrypt/config/nginx/site-confs/
-cp -f nginx.conf ~/appdata/letsencrypt/config/nginx/site-confs/default
+mkdir -p ~/appdata/letsencrypt/nginx/site-confs/
+cp -f nginx.conf ~/appdata/letsencrypt/nginx/site-confs/default
 /usr/local/bin/docker-compose up -d --force-recreate --remove-orphans
