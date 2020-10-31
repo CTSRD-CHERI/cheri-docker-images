889c5e963fff4870e4fdd64c6a693703ec4827f0
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-10-31 13:30:48 +0000

    Send email on failure

--- cheri-compiler-explorer/install.sh
+++ cheri-compiler-explorer/install.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p /root/appdata/letsencrypt/nginx/site-confs/
 cp -f "${DIR}/nginx.conf" /root/appdata/letsencrypt/nginx/site-confs/default
 cp -f "${DIR}/cheri-compiler-explorer.service" /etc/systemd/system/cheri-compiler-explorer.service
+cp -f "${DIR}/email-on-failure@.service" /etc/systemd/system/email-on-failure@.service
 systemctl daemon-reload
 systemctl enable cheri-compiler-explorer.service
 systemctl start cheri-compiler-explorer.service
