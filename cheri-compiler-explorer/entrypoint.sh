3a402aa9f4efe13fefaae51efcd680b3750c1a43
Jessica Clarke <jrtc27@jrtc27.com>
2022-01-24 14:49:09 +0000

    cheri-compiler-explorer: Preserve NODE_ENV through sudo
    Not needed for id but keeps things simple.

--- cheri-compiler-explorer/entrypoint.sh
+++ cheri-compiler-explorer/entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 # Work around docker-created volumes always being owned by root
 chown -Rv gcc-user:users /compiler-explorer/lib/storage/data
 cd /compiler-explorer
-sudo -u gcc-user id
-sudo -u gcc-user make "$@"
+SUDO="sudo -u gcc-user --preserve-env=NODE_ENV"
+$SUDO id
+$SUDO make "$@"
